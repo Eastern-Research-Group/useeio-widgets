a49fa2fde6ec5aa447fcd1755826e7bca20d3bda
Jonas Hernandez <jonas.hernandez@erg.com>
2024-03-15 11:02:31 -0500

    updated smart_sector_eeio.ts to dynamically update the ApexChart options

--- src/smartSectorChart/smart-sector-eeio.ts
+++ src/smartSectorChart/smart-sector-eeio.ts
@@ -14,86 +14,62 @@ export interface SmartSectorChartConfig {
 
 
 export class SmartSectorEEIO extends Widget {
+    chart:ApexCharts;
+    modelSmartSectorApi:WebModelSmartSector;
 
     constructor(private _chartConfig: SmartSectorChartConfig) {
         super();
+        this.modelSmartSectorApi = modelOfSmartSector({
+            endpoint: this._chartConfig.endpoint as string,
+            model: this._chartConfig.model.id() as string,
+            asJsonFiles: true
+    })
     }
 
-    async update() {
-      let modelSmartSector = modelOfSmartSector({
-               endpoint: this._chartConfig.endpoint as string,
-               model: this._chartConfig.model.id() as string,
-               asJsonFiles: true
-       })
+   async update() {}
 
-       let sectorContributionToImpactFinalGWP_AR6_20:SectorContributionToImpact[] = await modelSmartSector.sectorContributionToImpactGhg("final/GWP-AR6-20.json");
-       let sectorContributionToImpactFinalGWP_AR6_100:SectorContributionToImpact[] = await modelSmartSector.sectorContributionToImpactGhg("final/GWP-AR6-100.json");
-       let sectorContributionToImpactFinalSCC:SectorContributionToImpact[] = await modelSmartSector.sectorContributionToImpactGhg("final/Social-Cost-of-Carbon.json");
-      
-       let sectorContributionToImpactDirectGWP_AR6_20:SectorContributionToImpact[] = await modelSmartSector.sectorContributionToImpactGhg("direct/GWP-AR6-20.json");
-       let sectorContributionToImpactDirectGWP_AR6_100:SectorContributionToImpact[] = await modelSmartSector.sectorContributionToImpactGhg("direct/GWP-AR6-100.json");
-       let sectorContributionToImpactDirectSCC:SectorContributionToImpact[] = await modelSmartSector.sectorContributionToImpactGhg("direct/Social-Cost-of-Carbon.json");
-
-
-       let sectorMappingList:SectorMapping[] = await modelSmartSector.sectorMapping();  
-       let sortedSectorMappingByGroup:SectorMapping[] = sectorMappingList.sort((a: SectorMapping, b: SectorMapping): any => {
+   async init(graphName:string)
+   {
+    let sectorMappingList:SectorMapping[] = await this.modelSmartSectorApi.sectorMapping();  
+    let sortedSectorMappingByGroup:SectorMapping[] = sectorMappingList.sort((a: SectorMapping, b: SectorMapping): any => {
            return a.group.localeCompare(b.group);
        });
-       let uniqueSortedMappingGroupNoDuplicates:string[] = this.uniqueSortedMappingGroup(sortedSectorMappingByGroup);
+    let uniqueSortedMappingGroupNoDuplicates:string[] = this.uniqueSortedMappingGroup(sortedSectorMappingByGroup);
 
+    let sectorContributionToImpact:SectorContributionToImpact[] = await this.modelSmartSectorApi.sectorContributionToImpactGhg("final/"+graphName);
 
-       let optionsFinalGWP_AR6_20 = await this.getGraphs(sectorContributionToImpactFinalGWP_AR6_20, modelSmartSector,'GWP-AR6-20');
-       let optionsFinalGWP_AR6_100 = await this.getGraphs(sectorContributionToImpactFinalGWP_AR6_100, modelSmartSector,'GWP-AR6-100');
-       let optionsFinalSCC = await this.getGraphs(sectorContributionToImpactFinalSCC, modelSmartSector,'Social Cost of Carbon');
-       
-
-       let optionsDirectGWP_AR6_20 = await this.getGraphs(sectorContributionToImpactDirectGWP_AR6_20, modelSmartSector,'GWP-AR6-20');
-       let optionsDirectGWP_AR6_100 = await this.getGraphs(sectorContributionToImpactDirectGWP_AR6_100, modelSmartSector,'GWP-AR6-100');
-       let optionsDirectSCC = await this.getGraphs(sectorContributionToImpactDirectSCC, modelSmartSector,'Social Cost of Carbon');
-
-       let option20 = await calculate(optionsFinalGWP_AR6_20,this._chartConfig.model,uniqueSortedMappingGroupNoDuplicates,'GWP-AR6-20');
-       let option100 = await calculate(optionsFinalGWP_AR6_100,this._chartConfig.model,uniqueSortedMappingGroupNoDuplicates,'GWP-AR6-100');
-       let optionSCC = await calculate(optionsFinalSCC,this._chartConfig.model,uniqueSortedMappingGroupNoDuplicates,'Social Cost of Carbon');
-
-       let option20Direct = await calculate(optionsDirectGWP_AR6_20,this._chartConfig.model,uniqueSortedMappingGroupNoDuplicates,'GWP-AR6-20');
-       let option100Direct = await calculate(optionsDirectGWP_AR6_100,this._chartConfig.model,uniqueSortedMappingGroupNoDuplicates,'GWP-AR6-100');
-       let optionSCCDirect = await calculate(optionsDirectSCC,this._chartConfig.model,uniqueSortedMappingGroupNoDuplicates,'Social Cost of Carbon');
-
-       let chart20 = new ApexCharts(
-           document.querySelector(this._chartConfig.selector),
-           option20,
-       );
-      let chart100 = new ApexCharts(
-            document.querySelector(this._chartConfig.selector+"-100"),
-            option100,
-        );
-
-       let chartSCC = new ApexCharts(
-            document.querySelector(this._chartConfig.selector+"-SCC"),
-            optionSCC,
-        );
-
-       chart20.render();
-       chart100.render(); 
-       chartSCC.render();
-
-       let chart20Direct = new ApexCharts(
-        document.querySelector(this._chartConfig.selector+'_direct'),
-        option20Direct,
-    );
-    let chart100Direct = new ApexCharts(
-        document.querySelector(this._chartConfig.selector+"-100_direct"),
-        option100Direct,
+    let options = await this.getGraphs(sectorContributionToImpact, this.modelSmartSectorApi, graphName.replace('-'," "));
+    let option = await calculate(options,this._chartConfig.model,uniqueSortedMappingGroupNoDuplicates, graphName.replace('-'," "));
+    this.chart = new ApexCharts(
+        document.querySelector(this._chartConfig.selector),
+        option,
     );
 
-    let chartSCCDirect = new ApexCharts(
-        document.querySelector(this._chartConfig.selector+"-SCC_direct"),
-        optionSCCDirect,
-    );
+    this.chart.render();
+   }
 
-        chart20Direct.render();
-        chart100Direct.render(); 
-        chartSCCDirect.render();
+   async selectiveGraph(graphName:string,perspective:string)
+   {
+    let sectorContributionToImpact:SectorContributionToImpact[] = []
+    let sectorMappingList:SectorMapping[] = await this.modelSmartSectorApi.sectorMapping();  
+    let sortedSectorMappingByGroup:SectorMapping[] = sectorMappingList.sort((a: SectorMapping, b: SectorMapping): any => {
+           return a.group.localeCompare(b.group);
+       });
+    let uniqueSortedMappingGroupNoDuplicates:string[] = this.uniqueSortedMappingGroup(sortedSectorMappingByGroup);
+    if(perspective === 'final')
+    {
+        sectorContributionToImpact = await this.modelSmartSectorApi.sectorContributionToImpactGhg("final/"+graphName);
+    }
+    else if(perspective === 'direct')
+    {
+        sectorContributionToImpact = await this.modelSmartSectorApi.sectorContributionToImpactGhg("direct/"+graphName);
+
+    }
+
+    let options = await this.getGraphs(sectorContributionToImpact, this.modelSmartSectorApi, graphName.replace('-'," "));
+    let option = await calculate(options,this._chartConfig.model,uniqueSortedMappingGroupNoDuplicates, graphName.replace('-'," "));
+    
+    this.chart.updateOptions(option);
    }
 
     private uniqueSortedMappingGroup(sortedSectorMappingByGroup:SectorMapping[]) : string[]{
