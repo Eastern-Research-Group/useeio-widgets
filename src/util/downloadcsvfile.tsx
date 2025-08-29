765978e0d975e5ac7f4de99de3dfade0afb258bd
Jhernandez29 <jonashernandez@erg.com>
2025-08-29 15:59:16 -0500

    Added lazy loading for csv files.

--- src/util/downloadcsvfile.tsx
+++ src/util/downloadcsvfile.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Papa, { ParseResult } from "papaparse";
 import { Button, makeStyles } from "@material-ui/core";
+import { CircularProgress, Typography } from '@material-ui/core';
 const useStyles = makeStyles({
   container: {
     margin: "2em .5em",
@@ -17,7 +18,7 @@ export interface DownloadCSVButtonProps {
   fileObjects: {
     filename: string,
     perspective: string,
-    selectorList?:string[],
+    selectorList?: string[],
     sector?: string
   }
 }
@@ -25,14 +26,17 @@ export interface DownloadCSVButtonProps {
 const DownloadCSVButton: React.FC<DownloadCSVButtonProps> = ({ fileObjects }) => {
   const [records, setRecords] = useState<RecordData[]>([]);
   const [headers, setHeaders] = useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
+
   const classes = useStyles();
   let fileName = fileObjects.filename;
   fileName += ((fileObjects.perspective == "final") ? "-Point-of-Consumption.csv" : "-Supply-Chain.csv");
-  useEffect(() => {
-    axios
-      .get<string>(`./downloadCsvFiles/${fileName}`, { responseType: "text" })
-      .then((res) => {
-        const parsed: ParseResult<RecordData> = Papa.parse<RecordData>(
+  async function getCVSfile(){
+    setLoading(true)
+    try{
+      const res =  await axios
+      .get<string>(`./downloadCsvFiles/${fileName}`, { responseType: "text" });
+      const parsed: ParseResult<RecordData> = Papa.parse<RecordData>(
           res.data,
           {
             header: true,
@@ -44,47 +48,53 @@ const DownloadCSVButton: React.FC<DownloadCSVButtonProps> = ({ fileObjects }) =>
           setHeaders(parsed.meta.fields);
         }
         setRecords(parsed.data);
-      }).catch( error => {
-        console.error('Error:',error)
+    }
+    catch (error) {
+      console.error("Error:",error)
         setRecords([])
-      }
-    )
-  }, [fileObjects.filename, fileObjects.sector, fileObjects.perspective]);
+    }
+    finally{
+        setLoading(false)
+    }
+  };
 
-  const handleDownload = () => {
-    if(records.length > 0){
-    let downloadFilename:string = ''
-    let filtered:RecordData[] = []
+  const handleDownload = async () => {
+    let downloadFilename: string = ''
+    let filtered: RecordData[] = []
+    let url
     if (fileObjects.sector) {
-        downloadFilename =  `${fileObjects.sector}_${fileName}`;
+      await getCVSfile();
+      downloadFilename = `${fileObjects.sector}_${fileName}`;
       filtered = records.filter(
         (row) => row["sector"] === fileObjects.sector
       )
+      const csv = Papa.unparse({
+        fields: headers,
+        data: filtered.map((row) => headers.map((h) => row[h] ?? "")),
+      });
+      const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+      url = URL.createObjectURL(blob);
     }
-    else if(fileObjects.selectorList)
-    {
-      downloadFilename =  `Custom_Sector_List_${fileName}`;
+    else if (fileObjects.selectorList) {
+      await getCVSfile();
+      downloadFilename = `Custom_Sector_List_${fileName}`;
       fileObjects.selectorList.forEach(
         sector => {
           filtered.push(...records.filter(
-        (row) => row["sector"] === sector
-      ))
+            (row) => row["sector"] === sector
+          ))
         }
       )
+      const csv = Papa.unparse({
+        fields: headers,
+        data: filtered.map((row) => headers.map((h) => row[h] ?? "")),
+      });
+      const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+      url = URL.createObjectURL(blob);
     }
     else {
-    downloadFilename = `${fileName}`
-
-      filtered = records
+      url = `./downloadCsvFiles/${fileName}`;
     }
-
-    const csv = Papa.unparse({
-      fields: headers,
-      data: filtered.map((row) => headers.map((h) => row[h] ?? "")),
-    });
-
-    const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
-    const url = URL.createObjectURL(blob);
 
     const link = document.createElement("a");
     link.href = url;
@@ -92,13 +102,35 @@ const DownloadCSVButton: React.FC<DownloadCSVButtonProps> = ({ fileObjects }) =>
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-  }
-};
+  };
 
   return (
-    <Button variant="outlined" color="primary" onClick={handleDownload} className={classes.container}>
-      Download Data (CSV)
-    </Button>
+    <div>
+      {loading ?
+        <div style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          backgroundColor: 'rgba(255, 255, 255, 0.8)',
+          zIndex: 9999,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+          <CircularProgress size={70} />
+          <Typography style={{ marginTop: 25 }}>
+            Creating CSV File...
+          </Typography>
+        </div>
+        : ""
+      }
+      <Button variant="outlined" color="primary" onClick={handleDownload} className={classes.container}>
+        Download Data (CSV)
+      </Button>
+    </div>
   );
 };
 
